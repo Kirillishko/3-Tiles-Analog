6d38800bcb154dfc0ac599d38e092f0930a5d40c
Kirillishko <shumaev.kirill@gmail.com>
2022-11-16 09:12:07 +0500

    q

--- Assets/Scripts/Data/LevelData.cs
+++ Assets/Scripts/Data/LevelData.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Data", fileName = "Icons")]
 public class LevelData : ScriptableObject
 {
-    [SerializeField] private Sprite _backgroundIcon;
     [SerializeField] private List<Sprite> _tileIcons;
+    [SerializeField] private Sprite _backgroundIcon;
+    [SerializeField] private Sprite _specialTileIcon;
     [SerializeField] private AudioClip _clip;
-    
-    public List<Sprite> TileIcons => _tileIcons;
+
+    public IReadOnlyList<Sprite> TileIcons => _tileIcons;
     public Sprite BackgroundIcon => _backgroundIcon;
+    public Sprite SpecialTileIcon=> _specialTileIcon;
     public AudioClip Clip => _clip;
 }
---
+++ Assets/Scripts/Data/LevelFormSaver.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public class LevelFormSaver : MonoBehaviour
+{
+    private const string Extension = ".txt";
+    
+    [SerializeField] private string _fileName;
+    [SerializeField] private TileLayer[] _layers;
+    
+    [ContextMenu("SaveLevel")]
+    public void SaveLevel()
+    {
+        string path = Application.dataPath + "/Levels/" + _fileName + Extension;
+        var writer = new StreamWriter(path);
+
+        for (int i = 0; i < _layers.Length; i++)
+        {
+            writer.WriteLine($"l{i}");
+            var layer = _layers[i];
+
+            foreach (var tile in layer.GetComponentsInChildren<Tile>())
+            {
+                var position = tile.transform.position;
+                writer.WriteLine($"{position.x} {position.y}");
+            }
+        }
+
+        writer.Close();
+    }
+}
